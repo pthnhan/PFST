848670853b5819ca4a10f9bdefaec4134a9e245e
pthnhan <nhanmath7@gmail.com>
2022-03-01 11:58:01 +0700

    update

--- package/pfs/_pfs.py
+++ package/pfs/_pfs.py
@@ -7,14 +7,14 @@ from pfs.parallel_reforward import get_parallel_reforward
 from pfs.parallel_R_argmax import get_parallel_get_argmax
 import multiprocessing
 
-R_backward = []
+R_backward_argmax = []
 
-def get_R_backward(X, R_minus_f, y, ni, C):
-    trace_univariate = np.array([univariate(X[:, i], y, ni, C) for i in np.arange(X.shape[1])])
-    R = list(np.array([np.argmax(trace_univariate)]))
-    return [R_minus_f[i] for i in R]
+def get_R_backward_argmax(X, block, R, y, ni, C):
+    compute_t = lambda i: mult_trace(X[:, np.delete(R, i)], y, ni, C)
+    block_trace = np.array([compute_t(R.index(block[i])) for i in range(len(block))])
+    return [block[np.argmax(block_trace)]]
 
-def parallel_forward_backward_algo(X, y, n_workers, alpha=0.05, beta=0.01):
+def get_parallel_backward_argmax(X, y, n_workers, alpha=0.05):
     R_argmax = get_parallel_get_argmax(X, y, n_workers)
     R_forward_dropping = get_parallel_forward_dropping(X, y, n_workers, alpha)
     R_Reforward = get_parallel_reforward(X, y, n_workers, alpha)
@@ -24,24 +24,21 @@ def parallel_forward_backward_algo(X, y, n_workers, alpha=0.05, beta=0.01):
     C = len(np.unique(y))
     ni = np.array([np.sum(y == cl) for cl in np.arange(C)])
     for block in blocks:
-        R_minus_f = np.setdiff1d(R, block)
-        X_block = X[:, R_minus_f]
-        p.apply_async(get_R_backward,
-                      args = (X_block, R_minus_f, y, ni, C),
-                      callback = save_R_backward)
+        p.apply_async(get_R_backward_argmax,
+                      args = (X, block, R, y, ni, C),
+                      callback = save_R_backward_argmax)
     p.close()
     p.join()
-    return R_backward
+    return list(set(R_backward_argmax))
 
-def save_R_backward(R):
-    R_backward.extend(R)
+def save_R_backward_argmax(R):
+    R_backward_argmax.extend(R)
 
-
-if __name__ == '__main__':
-    from sklearn import datasets
-    breastcc = datasets.load_breast_cancer()
-    X, y = breastcc.data, breastcc.target.ravel()
-    a = parallel_forward_backward_algo(X, y, n_workers = 2, alpha = 0.05, beta = 0.01)
-
+def get_pfs(X, y, n_workers=5, alpha=0.05, beta=0.01, gamma=0.05):
+    R = get_parallel_backward_argmax(X, y, n_workers, alpha)
+    C = len(np.unique(y))
+    ni = np.array([np.sum(y == cl) for cl in np.arange(C)])
+    R_selected = backward(X, R, y, ni, C, beta)
+    return R_selected
 
 
